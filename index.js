640e3b5a7b07939ad316a0ee4153945cd61ed579
Nikolai <nkolom30@gmail.com>
2020-06-30 17:02:08 +0600

    Improved Error Handling

--- index.js
+++ index.js
@@ -20,18 +20,18 @@ app.post('/signup', async (req, res) => {
     var login=req.body.login;
     var password=req.body.password;
     if(!login || !password){
-      throw "Provide login and password correctly";
+      throw new Error('Provide login and password correctly');
     }
     checkUsersLogin = await client.query("SELECT * FROM \"User\" WHERE login = $1", [login]);
     if(checkUsersLogin.rows.length != 0){
-      throw "Change your login"
+      throw new Error('Change your login');
     }
 
     var passwordHash = await bcrypt.hash(password, 10);
     await client.query("INSERT INTO \"User\" (login, password) values($1, $2)", [login, passwordHash]);
     res.status(201).send("Inserted");
-  }catch(error){
-    res.status(200).send(error);
+  }catch(err){
+    res.status(400).send({error: err.message});
   }
 })
 
@@ -42,21 +42,21 @@ app.post('/signin', async (req, res) => {
 
     var userInfo = await client.query("SELECT * FROM \"User\" WHERE login = $1", [login]);
     if( userInfo.rows.length == 0){
-      throw "Login or Password is incorrect";
+      throw new Error('Login or Password is incorrect');
     }
     var userId =  userInfo.rows[0].id;
     var isPasswordCorrect = await bcrypt.compare(password, userInfo.rows[0].password);
     if(!isPasswordCorrect){
-      throw "Login or Password is incorrect";
+      throw new Error('Login or Password is incorrect');
     }
     var salt = CryptoJS.lib.WordArray.random(128 / 8);
     var key128Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 128 / 32});
 
     await client.query("INSERT INTO \"Session\" (key, user_id) values($1, $2)", [key128Bits.toString(), userId]);
 
-    res.status(200).send(JSON.parse("{\"session_key\": \"" + key128Bits + "\"}"));
-  }catch(error){
-    res.status(400).send(error);
+    res.status(200).send({session_key: key128Bits.toString()});
+  }catch(err){
+    res.status(400).send({error: err.message});
   }
 })
 
@@ -64,10 +64,10 @@ app.post('/signin', async (req, res) => {
 
 app.get('/getCategories', async (req, res) => {
   try{
-    var categories = await client.query("SELECT * FROM \"Category\"");
+    var categories = await client.query('SELECT * FROM "Category"');
     res.status(200).send(categories.rows);
-  }catch(error){
-    res.status(200).send(error);
+  }catch(err){
+    res.status(200).send({error: err.message});
   }
 })
 
@@ -79,8 +79,8 @@ app.post('/createProduct', async (req, res) => {
     await client.query("INSERT INTO \"Product\" (name, category_id) VALUES ( $1, $2)", [name, category_id]);
     await client.query("UPDATE \"Category\" SET category_count = category_count + 1 WHERE id = $1", [category_id]);
     res.status(200).send("Inserted");
-  }catch(error){
-    res.status(400).send(error);
+  }catch(err){
+    res.status(400).send({error: err.message});
   }
 })
 
@@ -91,7 +91,7 @@ app.post('/deleteProduct', async (req, res) => {
 
     var product = await client.query("SELECT * FROM \"Product\" WHERE id = $1", [id]);
     if(product.rows.length == 0){
-      throw "Product Not Found";
+      throw new Error('Product Not Found');
     }
     category_id = product.rows[0].category_id;
 
@@ -99,8 +99,8 @@ app.post('/deleteProduct', async (req, res) => {
     await client.query("DELETE FROM \"Product\" WHERE id = $1", [id]);
     await client.query("UPDATE \"Category\" SET  category_count =  category_count - 1 WHERE id = $1", [category_id]);
     res.status(200).send("Product Deleted");
-  }catch(error){
-    res.status(400).send(error);
+  }catch(err){
+    res.status(400).send({error: err.message});
   }
 })
 
@@ -114,22 +114,13 @@ app.get('/getProducts', async (req, res) => {
       var productsList = await client.query("SELECT * FROM \"Product\"");
     }
     res.status(200).send(productsList.rows);
-  }catch(error){
-    res.status(400).send(error);
+  }catch(err){
+    res.status(400).send({error: err.message});
   }
 })
 
 
-function CheckKey(key, callback) {
-  client.query("SELECT \"User\".id FROM \"Session\" " +
-               " INNER JOIN \"User\" ON \"User\".id = \"Session\".user_id " + 
-               " WHERE \"Session\".key = $1 AND \"Session\".creation_date + interval '10 minutes' >= now()",
-               [key.toString()], function (err, result) {
-    return callback(err, result);
-  });
-}
-
-async function ChceckKey(key) {
+async function CheckKey(key) {
   var userId = await client.query("SELECT \"User\".id FROM \"Session\" " +
                " INNER JOIN \"User\" ON \"User\".id = \"Session\".user_id " + 
                " WHERE \"Session\".key = $1 AND \"Session\".creation_date + interval '10 minutes' >= now()",
@@ -141,17 +132,17 @@ app.post('/addToFavorites', async(req, res) =>{
   try{
   var product_id = req.body.product_id;
   var key = req.body.session_key;  
-  var userId = await ChceckKey(key.toString());
+  var userId = await CheckKey(key.toString());
 
   if(userId.err)
     throw err;
   else if(userId.rows.length == 0)
-    throw "Login first"
+    throw new Error('Session expire or does not exist');
 
   await client.query("INSERT INTO \"Favorite\" (user_id, product_id) VALUES ($1, $2)", [userId.rows[0].id, product_id]);
-  res.send("Inserted");
+  res.status(200).send("Inserted");
   }catch(err){
-    res.send(err);
+    res.status(400).send({error: err.message});
   }
 })
 
@@ -159,12 +150,12 @@ app.post('/addToFavorites', async(req, res) =>{
 app.get('/getFavorites', async(req, res) => {
   try{
   var key = req.body.session_key;
-  var userId = await ChceckKey(key.toString());
+  var userId = await CheckKey(key.toString());
 
   if(userId.err)
     throw err;
   else if(userId.rows.length == 0)
-    throw "Login first"
+    throw new Error('Session expire or does not exist');
   
   var listOfFavorites = await client.query("SELECT \"Product\".id, \"Product\".name" +
                                       " FROM \"Favorite\" "+
@@ -174,8 +165,8 @@ app.get('/getFavorites', async(req, res) => {
 
   res.status(200).send(listOfFavorites.rows);
 
-  }catch(error){
-    res.status(400).send(error);
+  }catch(err){
+    res.status(400).send({error: err.message});
   }
 })
 
